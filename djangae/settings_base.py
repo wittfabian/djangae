a1658dc365a8fed3ec4d64a280d1a901fce9d7eb
Luke Benstead <kazade@gmail.com>
2014-01-09 20:38:24 +0000

    Add missing files

---
+++ djangae/settings_base.py
@@ -0,0 +1,7 @@
+
+DEFAULT_FILE_STORAGE = 'djangae.storage.BlobstoreStorage'
+FILE_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024
+FILE_UPLOAD_HANDLERS = (
+    'djangae.storage.BlobstoreFileUploadHandler',
+    'django.core.files.uploadhandler.MemoryFileUploadHandler',
+)
