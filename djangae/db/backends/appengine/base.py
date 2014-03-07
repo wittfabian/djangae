5d008f406512938cf9fe0740f7d50c688ccecb70
chargrizzle <chargrizzled@gmail.com>
2014-03-07 10:09:38 +0000

    updates working

--- djangae/db/backends/appengine/base.py
+++ djangae/db/backends/appengine/base.py
@@ -32,6 +32,7 @@ from .commands import (
     SelectCommand,
     InsertCommand,
     FlushCommand,
+    UpdateCommand,
     get_field_from_column
 )
 
@@ -175,6 +176,8 @@ def django_instance_to_entity(connection, model, fields, raw, instance):
 
     field_values = {}
     primary_key = None
+    
+    # primary.key = self.model._meta.pk
     for field in fields:
         value, is_primary_key = value_from_instance(instance, field)
         if is_primary_key:
@@ -240,9 +243,34 @@ class Cursor(object):
 
                 entity[pk_column] = key.id_or_name()
                 cache_entity(sql.model, entity)
-
         else:
+            import pdb;pdb.set_trace() 
             raise RuntimeError("Can't execute traditional SQL: '%s'", sql)
+
+        if isinstance(sql, UpdateCommand):
+            # Because UpdateCommand is a subclass of SelectCommand, the behavior of
+            # isinstance will return True for both this check and the previous
+            # SelectCommand check
+            self.connection.queries.append(sql)
+            results = self.last_select_command.results
+            entities = []
+            for result in results:
+                for field, param, value in self.last_select_command.values:
+                    result[field.name] = value
+                    entities.append(result)
+            self.returned_ids = datastore.Put(entities)
+            #Now cache them, temporarily to help avoid consistency errors
+            for key, entity in zip(self.returned_ids, entities):
+                pk_column = sql.model._meta.pk.column
+
+                #If there are parent models, search the parents for the
+                #first primary key which isn't a relation field
+                for parent in sql.model._meta.parents.keys():
+                    if not parent._meta.pk.rel:
+                        pk_column = parent._meta.pk.column
+
+                entity[pk_column] = key.id_or_name()
+                cache_entity(sql.model, entity)
 
     def fix_fk_null(self, query, constraint):
         alias = constraint.alias
