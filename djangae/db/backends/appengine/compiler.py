f6fb8d5ae39fd1cb7964b09b9df8fe766d0c441b
Adam Alton <adamalton@gmail.com>
2014-04-27 13:42:11 +0100

    Re-factoring things to pave the way for supporting querying for poly models.  Starting with moving `validate_query_is_possible` into the `SelectCommand` so that we can tell whether or not we're querying for a poly model.

--- djangae/db/backends/appengine/compiler.py
+++ djangae/db/backends/appengine/compiler.py
@@ -1,76 +1,33 @@
+#LIBRARIES
 from django.db.models.sql import compiler
-from djangae.db.backends.appengine.query import Query
-from django.db.models.sql.datastructures import EmptyResultSet
-from django.db.models.sql.constants import MULTI, SINGLE, GET_ITERATOR_CHUNK_SIZE
-from django.db import connections
-from django.conf import settings
-
 #Following two ImportError blocks are for < 1.6 compatibility
 try:
     from django.db.models.sql.compiler import SQLDateCompiler as DateCompiler
 except ImportError:
     class DateCompiler(object):
         pass
-
 try:
     from django.db.models.sql.compiler import SQLDateTimeCompiler as DateTimeCompiler
 except ImportError:
     class DateTimeCompiler(object):
         pass
 
-from .base import django_instance_to_entity, NotSupportedError, CouldBeSupportedError
+#DJANGAE
+from djangae.db.backends.appengine.query import Query
 from .commands import InsertCommand, SelectCommand, UpdateCommand, DeleteCommand
 
-from google.appengine.api import datastore
 
-def validate_query_is_possible(query):
-    """
-        Need to check the following:
 
-        - The query only has one inequality filter
-        - The query does no joins
-        - The query ordering is compatible with the filters
-    """
-
-    #Check for joins
-    if query.count_active_tables() > 1:
-        raise NotSupportedError("""
-            The appengine database connector does not support JOINs. The requested join map follows\n
-            %s
-        """ % query.join_map)
-
-    if query.aggregates:
-        if query.aggregates.keys() == [ None ]:
-            if query.aggregates[None].col != "*":
-                raise NotSupportedError("Counting anything other than '*' is not supported")
-        else:
-            raise NotSupportedError("Unsupported aggregate query")
-
-    #This is a hack! Django < 1.7 sets an extra_select of a = 1 in has_results. In 1.7 this has
-    #been moved to the compiler.
-    if query.extra_select and query.extra_select != {'a': (u'1', [])}:
-        raise CouldBeSupportedError("The appengine connector currently doesn't support extra_select, it can be implemented though")
-                
 class SQLCompiler(compiler.SQLCompiler):
     query_class = Query
 
     def as_sql(self):
-
-        validate_query_is_possible(self.query)
-
-
-        #where = self.query.where.as_sql(
-        #    qn=self.quote_name_unless_alias,
-        #    connection=self.connection
-        #)
-
         select = SelectCommand(
             self.connection,
             self.query
         )
-
-        #print(where)
         return (select, [])
+
 
 class SQLInsertCompiler(compiler.SQLInsertCompiler, SQLCompiler):
     def __init__(self, *args, **kwargs):
