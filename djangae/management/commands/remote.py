6eccaf189447ef142c57016ef2ca9ffe38246419
Charlie Morgan <chargrizzled@gmail.com>
2014-05-12 12:36:01 +0100

    changed remote to remote shell, can also chain commands

--- djangae/management/commands/remote.py
+++ djangae/management/commands/remote.py
@@ -1,20 +1,24 @@
-from django.core.management.commands.shell import Command as DjangoCommand
+# from django.core.management.commands.shell import Command as DjangoCommand
+from django.core.management.base import BaseCommand, OutputWrapper
+from django.core.management import execute_from_command_line
 from django.conf import settings
 
-import os
+import os, sys
 
-class Command(DjangoCommand):
-    
+class Command(BaseCommand):
+
     def __init__(self, *args, **kwargs):
         from djangae.boot import setup_paths, setup_datastore_stubs
         setup_paths()
         super(Command, self).__init__(*args, **kwargs)
 
-    def handle_noargs(self, **options):
+    def run_from_argv(self, argv):
         from google.appengine.ext.remote_api import remote_api_stub
         from google.appengine.tools import appengine_rpc
         import getpass
         from djangae.boot import find_project_root
+
+        self.stdout = OutputWrapper(sys.stdout)
 
         def auth_func():
             return (raw_input('Google Account Login:'), getpass.getpass('Password:'))
@@ -24,14 +28,12 @@ class Command(DjangoCommand):
         app_id = app_yaml.split("application:")[1].lstrip().split()[0]
 
         self.stdout.write("Opening Remote API connection to {0}...\n".format(app_id))
-        
         remote_api_stub.ConfigureRemoteApi(None,
-            '/_ah/remote_api', 
-            auth_func, 
-            servername='{0}.appspot.com'.format(app_id), 
+            '/_ah/remote_api',
+            auth_func,
+            servername='{0}.appspot.com'.format(app_id),
             secure=True,
         )
-
         self.stdout.write("...Connection established...have a nice day :)\n".format(app_id))
-
-        super(Command, self).handle_noargs(**options)
+        argv = argv[:1] + argv[2:]
+        execute_from_command_line(argv)
