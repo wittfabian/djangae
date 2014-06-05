cad9e445d82ab2417f9636f14a553423805d75a6
Luke Benstead <lukeb@potatolondon.com>
2014-06-05 10:33:13 +0100

    Dismantle the sandbox to allow SQLite, subprocess and select

--- djangae/wsgi.py
+++ djangae/wsgi.py
@@ -1,8 +1,62 @@
 from .boot import setup_paths, on_production
 
 class DjangaeApplication(object):
-    @staticmethod
-    def fix_sandbox():
+    def fix_subprocess_module(self):
+        """
+            Making the subprocess module work on the dev_appserver is hard work
+            so I've isloated it all here
+        """
+        if on_production():
+            return
+
+        import sys
+        from google.appengine import dist27
+        from google.appengine.tools.devappserver2.python import sandbox
+
+        if 'fcntl' not in sandbox._WHITE_LIST_C_MODULES:
+            sandbox._WHITE_LIST_C_MODULES.extend([
+                'fcntl'
+            ])
+
+        if "subprocess" in dist27.MODULE_OVERRIDES:
+            dist27.MODULE_OVERRIDES.remove("subprocess")
+
+            if "subprocess" in sys.modules:
+                del sys.modules["subprocess"]
+
+
+            for finder in sys.meta_path:
+                if isinstance(finder, sandbox.ModuleOverrideImportHook):
+                    del finder.policies['os']
+
+            if "os" in sys.modules:
+                del sys.modules["os"]
+
+        if "select" in dist27.MODULE_OVERRIDES:
+            dist27.MODULE_OVERRIDES.remove("select")
+            if "select" not in sandbox._WHITE_LIST_C_MODULES:
+                sandbox._WHITE_LIST_C_MODULES.extend(["select"])
+
+            if "select" in sys.modules:
+                del sys.modules["select"]
+
+    def fix_sandbox(self):
+        """
+            This is the nastiest thing in the world...
+
+            This WSGI middleware is the first chance we get to hook into anything. On the dev_appserver
+            at this point the Python sandbox will have already been initialized. The sandbox replaces stuff
+            like the subprocess module, and the select module. As well as disallows _sqlite3. These things
+            are really REALLY useful for development.
+
+            So here we dismantle parts of the sandbox. Firstly we add _sqlite3 to the allowed C modules.
+
+            Next, we manipulate the sandbox to allow the default python subprocess and select modules to function.
+
+            This only happens on the dev_appserver, it would only die on live. Everything is checked so that
+            changes are only made if they haven't been made already.
+        """
+
         if on_production():
             return
 
@@ -15,9 +69,14 @@ class DjangaeApplication(object):
                 '_sqlite3'
             ])
 
+        #Fix up the subprocess module
+        self.fix_subprocess_module()
+
+
     def __init__(self, application):
         setup_paths()
         self.wrapped_app = application
 
     def __call__(self, environ, start_response):
+        self.fix_sandbox()
         return self.wrapped_app(environ, start_response)
