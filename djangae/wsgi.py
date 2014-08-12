018943a2ae601ba1d39e7978f112ccf88653c622
Luke Benstead <kazade@gmail.com>
2014-08-12 18:24:50 +0100

    Ensure users don't place django apps after djangae in INSTALLED_APPS which allows us to always override what we need

--- djangae/wsgi.py
+++ djangae/wsgi.py
@@ -75,6 +75,15 @@ class DjangaeApplication(object):
 
 
     def __init__(self, application):
+        from django.conf import settings
+        from django.core.exceptions import ImproperlyConfigured
+
+        for app in settings.INSTALLED_APPS[::-1]:
+            if app.startswith("django"):
+                raise ImproperlyConfigured("You must place 'djangae' after 'django' apps in installed apps")
+            elif app == "djangae":
+                break
+
         self.wrapped_app = application
 
     def __call__(self, environ, start_response):
