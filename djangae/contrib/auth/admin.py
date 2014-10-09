d99fe902dc915a1adddc06950334ec5d50db804a
Adam Alton <adamalton@gmail.com>
2014-10-09 21:23:12 +0100

    Register the correct user model with the Django admin, but only if it's one of the concrete Djangae user models.
    Tickle #49.

--- djangae/contrib/auth/admin.py
+++ djangae/contrib/auth/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-from djangae.contrib.auth.models import User
+from djangae.contrib.auth.models import GaeUser, GaeDatastoreUser
 
-admin.site.register(User)
+# Only register the user model with the admin if it is a Djangae model.  If a different/custom user
+# model is in use, then allow that app to deal with the Django admin.
+
+user_model = get_user_model()
+if user_model in (GaeUser, GaeDatastoreUser):
+    # Note that we don't need Django's usual custom Admin class with the password change form because
+    # users created through the App Engine users API do not have passwords stored in our DB.
+    admin.site.register(user_model)
