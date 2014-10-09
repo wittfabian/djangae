52d22471c6226c07a6f2dd9d4f0470b7655e7c28
Adam Alton <adamalton@gmail.com>
2014-10-09 22:21:51 +0100

    Fix the `AUTH_USER_MODEL` value in the example auth settings file.
    Touch #49.

--- djangae/contrib/gauth/settings.py
+++ djangae/contrib/gauth/settings.py
@@ -3,5 +3,5 @@ AUTHENTICATION_BACKENDS = (
     'djangae.contrib.gauth.backends.AppEngineUserAPI',
 )
 
-AUTH_USER_MODEL = 'djangae.User'
+AUTH_USER_MODEL = 'djangae.GaeDatastoreUser'
 LOGIN_URL = 'djangae_login_redirect'
