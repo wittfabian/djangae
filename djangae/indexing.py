abb641293a73938dc2b5a981d00c4ed4c7461410
benvand <benjaminfvandersteen@gmail.com>
2014-11-05 10:55:04 +0000

    Update indexing.py
    It looks like duplicates are extraneous in this index. This change will allow storage of longer values and reduce storage/ iteration on lookup if the value to be indexed contains any duplication. 
    Works for ContainsIndexer and IContainsIndexer index classes. 
    
    The indexer can now index a string  made up of 500 of the same character in an index list 500 long now too hence the <= 500:
    ```
    >>> value=''.join([str(0) for i in range(500)])
    >>> len(value)
    500
    >>> length=len(value)
    >>> len(list(set([value[i:j + 1] for i in xrange(length) for j in xrange(i, length)])))
    500
    >>> len([value[i:j + 1] for i in xrange(length) for j in xrange(i, length)])
    125250
    ```

--- djangae/indexing.py
+++ djangae/indexing.py
@@ -207,13 +207,13 @@ class WeekDayIndexer(Indexer):
 
 class ContainsIndexer(Indexer):
     def validate_can_be_indexed(self, value):
-        return isinstance(value, basestring) and len(value) < 500
+        return isinstance(value, basestring) and len(value) <= 500
 
     def prep_value_for_database(self, value):
         result = []
         if value:
             length = len(value)
-            result = [value[i:j + 1] for i in xrange(length) for j in xrange(i, length)]
+            result = list(set([value[i:j + 1] for i in xrange(length) for j in xrange(i, length)]))
             if len(result) > 500:
                 raise ValueError("Can't index for contains query, this value has too many permuatations")
 
