b14cac78fbe7ad2b999a69779e9fedda9f89ed28
Luke Benstead <kazade@gmail.com>
2014-11-06 09:18:16 +0000

    Skip tests which could be supported, add expected failures for those that can't

--- djangae/test_runner.py
+++ djangae/test_runner.py
@@ -1,5 +1,24 @@
 from django.test.simple import DjangoTestSuiteRunner
 
+import unittest
+from unittest import TextTestResult
+
+from djangae.db.backends.appengine.dbapi import NotSupportedError, CouldBeSupportedError
+
+class SkipUnsupportedTestResult(TextTestResult):
+
+    def addError(self, test, err):
+        if err[0] == NotSupportedError:
+            self.addExpectedFailure(test, err)
+        elif err[0] == CouldBeSupportedError:
+            self.addSkip(test, "This test could be supported by Djangae, but currently isn't")
+        else:
+            super(SkipUnsupportedTestResult, self).addError(test, err)
 
 class DjangaeTestSuiteRunner(DjangoTestSuiteRunner):
-    pass
+    def run_suite(self, suite, **kwargs):
+        return unittest.TextTestRunner(
+            verbosity=self.verbosity,
+            failfast=self.failfast,
+            resultclass=SkipUnsupportedTestResult
+        ).run(suite)
