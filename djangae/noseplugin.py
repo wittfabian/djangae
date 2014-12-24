780f7f378c9b3443d145863709ed54810ecb7b0a
Grzes Furga <grfurga@google.com>
2014-12-24 11:44:14 +0100

    Refactoring to reuse testbed init code.

--- djangae/noseplugin.py
+++ djangae/noseplugin.py
@@ -1,9 +1,5 @@
-from django.conf import settings
-from djangae.utils import find_project_root
-
-from google.appengine.ext import testbed
-from google.appengine.datastore import datastore_stub_util
 from nose.plugins import Plugin
+from djangae.test_runner import init_testbed
 
 
 class DjangaePlugin(Plugin):
@@ -12,23 +8,7 @@ class DjangaePlugin(Plugin):
         pass
 
     def startTest(self, test):
-        use_scattered = not getattr(settings, "DJANGAE_SEQUENTIAL_IDS_IN_TESTS", False)
-
-        stub_kwargs = {
-            "init_datastore_v3_stub": {
-                "use_sqlite": True,
-                "auto_id_policy": testbed.AUTO_ID_POLICY_SCATTERED if use_scattered else testbed.AUTO_ID_POLICY_SEQUENTIAL,
-                "consistency_policy": datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=1)
-            },
-            "init_taskqueue_stub": {
-                "root_path": find_project_root()
-            }
-        }
-
-        self.bed = testbed.Testbed()
-        self.bed.activate()
-        for init_name in testbed.INIT_STUB_METHOD_NAMES.values():
-            getattr(self.bed, init_name)(**stub_kwargs.get(init_name, {}))
+        self.bed = init_testbed()
 
     def stopTest(self, test):
         self.bed.deactivate()
