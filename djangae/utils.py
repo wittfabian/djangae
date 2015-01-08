1445b6f2c0a0374c68267294b35fbe3d10fa7bf8
Luke Benstead <kazade@gmail.com>
2015-01-08 15:54:13 +0000

    Fix indentation and make the memoize decorator callable directly

--- djangae/utils.py
+++ djangae/utils.py
@@ -48,30 +48,32 @@ import collections
 import functools
 
 class memoized(object):
-   def __init__(self, func):
-      self.func = func
-      self.cache = {}
+    def __init__(self, func, *args):
+        self.func = func
+        self.cache = {}
+        self.args = args
 
-   def __call__(self, *args):
-      if not isinstance(args, collections.Hashable):
+    def __call__(self, *args):
+        args = self.args or args
+        if not isinstance(args, collections.Hashable):
          # uncacheable. a list, for instance.
          # better to not cache than blow up.
          return self.func(*args)
 
-      if args in self.cache:
-         return self.cache[args]
-      else:
-         value = self.func(*args)
-         self.cache[args] = value
-         return value
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
 
-   def __repr__(self):
-      '''Return the function's docstring.'''
-      return self.func.__doc__
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
 
-   def __get__(self, obj, objtype):
-      '''Support instance methods.'''
-      return functools.partial(self.__call__, obj)
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
 
 @memoized
 def find_project_root():
