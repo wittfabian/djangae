d10d6b99728d5a4cbf886ef42d7aaee5ad91f926
Charlie Morgan <chargrizzled@gmail.com>
2015-01-14 23:29:22 +0000

    Added a MapReduce pipeline class to make things a bit nicer

--- djangae/contrib/mappers/pipes.py
+++ djangae/contrib/mappers/pipes.py
@@ -4,16 +4,45 @@ from queryset import QueryDef
 import cPickle
 
 
-def start_django_orm_mapper(query_def, func, job_name, shards=3):
-    """ Wrapper function for launching a map job """
+class MapReduceTask(object):
+    """
 
-    params = {'querydef': str(cPickle.dumps(query_def))}
+        Mapreduce base class
+    """
+    shards = 3
+    query_def = None
+    pipeline = None
+    pipeline_class = MapperPipeline
+    job_name = 'testmap'
 
-    pipeline = MapperPipeline(
-        job_name, # job_name: job name as string.
-        func, # mapper_spec: specification of mapper to use.
-        input_reader_spec="djangae.contrib.mappers.readers.DjangoInputReader", # input_reader_spec: specification of input reader to read data from.
-        params=params, # mapper_params: parameters to use for mapper phase.
-        shards=3 # shards: number of shards to use as int.
-    )
-    pipeline.start()
+    def get_class_path(self):
+        return '{mod}.{cls}'.format(
+            mod=self.__class__.__module__,
+            cls=self.__class__.__name__,
+        )
+
+    def get_map_path(self):
+        return '{cls}.{func}'.format(
+            cls=self.get_class_path(),
+            func=self.map.__name__
+        )
+
+    @classmethod
+    def map(cls, entity):
+        raise NotImplementedError('You must supply a map function')
+
+    @classmethod
+    def reduce(key, values):
+        raise NotImplementedError('No reduce function defined, just running map stage')
+
+    def start(self):
+        params = {'querydef': str(cPickle.dumps(self.query_def))}
+
+        self.pipeline = self.pipeline_class(
+            self.job_name, # job_name: job name as string.
+            self.get_map_path(), # mapper_spec: specification of mapper to use.
+            input_reader_spec="djangae.contrib.mappers.readers.DjangoInputReader", # input_reader_spec: specification of input reader to read data from.
+            params=params, # mapper_params: parameters to use for mapper phase.
+            shards=3 # shards: number of shards to use as int.
+        )
+        self.pipeline.start()
