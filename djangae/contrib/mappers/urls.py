ca3e0439b51df55460777c930349039c0afed68f
Charlie Morgan <chargrizzled@gmail.com>
2015-01-15 11:04:21 +0000

    change urlconf to a generator

--- djangae/contrib/mappers/urls.py
+++ djangae/contrib/mappers/urls.py
@@ -3,6 +3,5 @@ from django.http import HttpResponse
 from mapreduce.main import create_handlers_map
 from djangae.utils import djangae_webapp
 
-# This should work but does not
-wrapped_urls = [(url_re.replace('.*/', '^', 1), djangae_webapp(func)) for url_re, func in create_handlers_map()]
+wrapped_urls = ((url_re.replace('.*/', '^', 1), djangae_webapp(func)) for url_re, func in create_handlers_map())
 urlpatterns = patterns('', *wrapped_urls)
