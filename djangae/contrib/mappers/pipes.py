76fdd1b0d3a9c8bb55fa7b4e26acb592c1c8181f
Charlie Morgan <chargrizzled@gmail.com>
2015-01-24 12:55:08 +0000

    now MapReduceTask uses the pipeline class properly, which allows us to chain them properly as pipeline was intended, also get access to the callback function

--- djangae/contrib/mappers/pipes.py
+++ djangae/contrib/mappers/pipes.py
@@ -1,10 +1,57 @@
 from mapreduce.mapper_pipeline import MapperPipeline
+from mapreduce import parameters
+from mapreduce import control
+from mapreduce import context
+from pipeline.util import for_name
+
+
+DJANGAE_MAPREDUCE_BASE_PATH = '/_ah/mapreduce'
+
+
+class DjangaeMapperPipeline(MapperPipeline):
+
+    def run(self, job_name, handler_spec, input_reader_spec, output_writer_spec=None, params=None, shards=None):
+        """
+            Overwritting this method allows us to pass the base_path properly, I know it's stupid but I think
+            this is the cleanest way that still gives us a working Pipeline that we can chain
+        """
+        if shards is None:
+          shards = parameters.config.SHARD_COUNT
+
+        mapreduce_id = control.start_map(
+            job_name,
+            handler_spec,
+            input_reader_spec,
+            params or {},
+            mapreduce_parameters={
+                "done_callback": self.get_callback_url(),
+                "done_callback_method": "GET",
+                "pipeline_id": self.pipeline_id,
+                "base_path": DJANGAE_MAPREDUCE_BASE_PATH
+            },
+            shard_count=shards,
+            output_writer_spec=output_writer_spec,
+            queue_name=self.queue_name,
+            )
+        self.fill(self.outputs.job_id, mapreduce_id)
+        self.set_status(console_url="%s/detail?mapreduce_id=%s" % (
+            (parameters.config.BASE_PATH, mapreduce_id)))
+
+    def callback(self, **kwargs):
+        """
+            Callback finish exists on the pipeline class, so we just use it as a nice
+            wrapper for the static method attached to the MapReduceTask
+        """
+        ctx = context.get()
+        params = ctx.mapreduce_spec.mapper.params
+        finish_func = params.get('_finish', None)
+        finish_func = for_name(finish_func)
+        return finish_func(**kwargs)
 
 
 class MapReduceTask(object):
     """
-
-        Mapreduce base class, inherit this in a statically defined class and
+        MapReduceTask base class, inherit this in a statically defined class and
         use .start() to run a mapreduce task
 
         You must define a staticmethod 'map' which takes in an arg of the entity being mapped over.
@@ -13,13 +60,14 @@ class MapReduceTask(object):
         You can pass any additional args and/or kwargs to .start(), which will then be passed into
         each call of .map() for you.
 
+        Overwrite 'finish' with a static definition for a finish callback
     """
     shard_count = 3
     pipeline_class = MapperPipeline # Defaults to MapperPipeline which just runs map stage
     job_name = None
     queue_name = 'default'
     output_writer_spec = None
-    base_path = '/_ah/mapreduce'
+    pipeline_base_path = '/_ah/mapreduce/pipeline'
     output_writer_spec = None
     mapreduce_parameters = {}
     countdown = None
@@ -62,43 +110,51 @@ class MapReduceTask(object):
             func=self.reduce.__name__
         )
 
+    def get_finish_path(self):
+        return '{cls}.{func}'.format(
+            cls=self.get_class_path(),
+            func=self.finish.__name__
+        )
+
     @staticmethod
     def map(entity, *args, **kwargs):
         """
-        Override this definition with a staticmethod map definition
-
+            Override this definition with a staticmethod map definition
         """
         raise NotImplementedError('You must supply a map function')
 
     @staticmethod
     def reduce(key, values):
-        pass
+        """
+            Override this with a static method for the reduce phase
+        """
+        raise NotImplementedError('You must supply a reduce function')
+
+    @staticmethod
+    def finish(**kwargs):
+        """
+            Override this with a static method for the finish callback
+        """
+        raise NotImplementedError('You must supply a finish function')
 
     def start(self, *args, **kwargs):
-        from mapreduce.control import start_map
         mapper_parameters = {
             'model': self.get_model_app_(),
             '_map': self.get_map_path(),
+            '_finish': self.get_finish_path(),
             'kwargs': kwargs,
             'args': args,
         }
 
-        start_map(
+        pipe = DjangaeMapperPipeline(
             self.job_name,
             'djangae.contrib.mappers.pipes.thunk_map',
             'djangae.contrib.mappers.readers.DjangoInputReader',
-            mapper_parameters,
-            shard_count=self.shard_count,
-            output_writer_spec=self.output_writer_spec,
-            mapreduce_parameters=None,
-            base_path=self.base_path,
-            queue_name=self.queue_name,
-            eta=self.eta,
-            countdown=self.countdown,
-            # hooks_class_name=None,
-            # _app=None,
-            # in_xg_transaction=False
+            params=mapper_parameters,
+            shards=self.shard_count
         )
+        pipe.start(base_path=self.pipeline_base_path)
+
 
 
 def thunk_map(x):
@@ -109,7 +165,6 @@ def thunk_map(x):
         more dynamic mappers
     """
     from mapreduce import context
-    from pipeline.util import for_name
     ctx = context.get()
     params = ctx.mapreduce_spec.mapper.params
     map_func = params.get('_map', None)
@@ -117,12 +172,3 @@ def thunk_map(x):
     kwargs = params.get('kwargs', {})
     map_func = for_name(map_func)
     return map_func(x, *args, **kwargs)
-
-# pipeline = self.pipeline_class(
-#     self.job_name, # job_name: job name as string.
-#     self.get_map_path(), # mapper_spec: specification of mapper to use.
-#     input_reader_spec="djangae.contrib.mappers.readers.DjangoInputReader", # input_reader_spec: specification of input reader to read data from.
-#     params=mapper_parameters, # mapper_params: parameters to use for mapper phase.
-#     shards=self.shards, # shards: number of shards to use as int.
-# )
-# pipeline.start(base_path='/_ah/mapreduce/pipeline')
