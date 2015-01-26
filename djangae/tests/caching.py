0708a0ebf54362a2ee08a1ede8cb321517cda1cc
Luke Benstead <kazade@gmail.com>
2015-01-26 18:52:38 +0000

    Write some tests for the caching functionality (all currently fail)

--- djangae/tests/caching.py
+++ djangae/tests/caching.py
@@ -1,10 +1,24 @@
+from djangae.contrib import sleuth
+from django.db import models
+from django.core.cache import cache
 from djangae.test import TestCase
+from djangae.db import unique_utils
+from djangae.db import transaction
 from djangae.db.backends.appengine.context import ContextStack
 from google.appengine.api import datastore
 
 class FakeEntity(dict):
+    COUNTER = 1
+
+    def __init__(self, data):
+        self.update(data)
+
     def key(self):
-        return datastore.Key.from_path("fake", 1)
+        try:
+            return datastore.Key.from_path("table", FakeEntity.COUNTER)
+        finally:
+            FakeEntity.COUNTER += 1
+
 
 class ContextStackTests(TestCase):
 
@@ -13,8 +27,7 @@ class ContextStackTests(TestCase):
 
         self.assertEqual({}, stack.top.cache)
 
-        entity = FakeEntity()
-        entity["bananas"] = 1
+        entity = FakeEntity({"bananas": 1})
 
         stack.top.cache_entity(["bananas:1"], entity)
 
@@ -38,8 +51,7 @@ class ContextStackTests(TestCase):
         self.assertEqual(2, stack.size)
         self.assertEqual(1, stack.staged_count)
 
-        updated = FakeEntity()
-        updated["bananas"] = 3
+        updated = FakeEntity({"bananas": 3})
 
         stack.top.cache_entity(["bananas:1"], updated)
 
@@ -52,9 +64,7 @@ class ContextStackTests(TestCase):
     def test_property_deletion(self):
         stack = ContextStack()
 
-        entity = FakeEntity()
-        entity["field1"] = "one"
-        entity["field2"] = "two"
+        entity = FakeEntity({"field1": "one", "field2": "two"})
 
         stack.top.cache_entity(["entity"], entity)
 
@@ -71,6 +81,16 @@ class ContextStackTests(TestCase):
 
 
 
+class CachingTestModel(models.Model):
+
+    field1 = models.CharField(max_length=255, unique=True)
+    comb1 = models.IntegerField(default=0)
+    comb2 = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = [
+            ("comb1", "comb2")
+        ]
 
 
 class MemcacheCachingTests(TestCase):
@@ -87,29 +107,204 @@ class MemcacheCachingTests(TestCase):
          - filter/get by anything else does not (eventually consistent)
     """
 
-    def test_save_caches_outside_transaction(self):
-        pass
+    def test_save_caches_outside_transaction_only(self):
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
 
-    def test_save_doesnt_cache_inside_transaction(self):
-        pass
+        identifiers = unique_utils.unique_identifiers_from_entity(CachingTestModel, FakeEntity(entity_data))
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+        instance = CachingTestModel.objects.create(**entity_data)
+
+        for identifier in identifiers:
+            self.assertEqual(entity_data, cache.get(identifier))
+
+        instance.delete()
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+
+        with transaction.atomic():
+            instance = CachingTestModel.objects.create(**entity_data)
+
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
 
     def test_save_wipes_entity_from_cache_inside_transaction(self):
-        pass
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        identifiers = unique_utils.unique_identifiers_from_entity(CachingTestModel, FakeEntity(entity_data))
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+        instance = CachingTestModel.objects.create(**entity_data)
+
+        for identifier in identifiers:
+            self.assertEqual(entity_data, cache.get(identifier))
+
+        with transaction.atomic():
+            instance.save()
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
 
     def test_consistent_read_updates_memcache_outside_transaction(self):
-        pass
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        identifiers = unique_utils.unique_identifiers_from_entity(CachingTestModel, FakeEntity(entity_data))
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+        CachingTestModel.objects.create(**entity_data)
+
+        for identifier in identifiers:
+            self.assertEqual(entity_data, cache.get(identifier))
+
+        cache.clear()
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+        CachingTestModel.objects.get() # Consistent read
+
+        for identifier in identifiers:
+            self.assertEqual(entity_data, cache.get(identifier))
 
     def test_eventual_read_doesnt_update_memcache(self):
-        pass
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        identifiers = unique_utils.unique_identifiers_from_entity(CachingTestModel, FakeEntity(entity_data))
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+        CachingTestModel.objects.create(**entity_data)
+
+        for identifier in identifiers:
+            self.assertEqual(entity_data, cache.get(identifier))
+
+        cache.clear()
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
+
+        CachingTestModel.objects.all()[0] # Inconsistent read
+
+        for identifier in identifiers:
+            self.assertIsNone(cache.get(identifier))
 
     def test_unique_filter_hits_memcache(self):
-        pass
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        original = CachingTestModel.objects.create(**entity_data)
+
+        with sleuth.watch("google.appengine.api.datastore.Query.Run") as datastore_query:
+            instance = CachingTestModel.objects.filter(field1="Apple").all()[0]
+            self.assertEqual(original, instance)
+
+        self.assertFalse(datastore_query.called)
+
+    def test_non_unique_filter_hits_datastore(self):
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        original = CachingTestModel.objects.create(**entity_data)
+
+        with sleuth.watch("google.appengine.api.datastore.Query.Run") as datastore_query:
+            instance = CachingTestModel.objects.filter(comb1=1).all()[0]
+            self.assertEqual(original, instance)
+
+        self.assertTrue(datastore_query.called)
 
     def test_get_by_key_hits_memcache(self):
-        pass
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        original = CachingTestModel.objects.create(**entity_data)
+
+        with sleuth.watch("google.appengine.api.datastore.Get") as datastore_get:
+            instance = CachingTestModel.objects.get(pk=original.pk)
+            self.assertEqual(original, instance)
+
+        self.assertFalse(datastore_get.called)
+
+    def test_get_by_key_hits_datastore_inside_transaction(self):
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        original = CachingTestModel.objects.create(**entity_data)
+
+        with sleuth.watch("google.appengine.api.datastore.Get") as datastore_get:
+            with transaction.atomic():
+                instance = CachingTestModel.objects.get(pk=original.pk)
+            self.assertEqual(original, instance)
+
+        self.assertTrue(datastore_get.called)
 
     def test_unique_get_hits_memcache(self):
-        pass
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        original = CachingTestModel.objects.create(**entity_data)
+
+        with sleuth.watch("google.appengine.api.datastore.Get") as datastore_get:
+            instance = CachingTestModel.objects.get(field1="Apple")
+            self.assertEqual(original, instance)
+
+        self.assertFalse(datastore_get.called)
+
+    def test_unique_get_hits_datastore_inside_transaction(self):
+        entity_data = {
+            "field1": "Apple",
+            "comb1": 1,
+            "comb2": "Cherry"
+        }
+
+        original = CachingTestModel.objects.create(**entity_data)
+
+        with sleuth.watch("google.appengine.api.datastore.Get") as datastore_get:
+            with transaction.atomic():
+                instance = CachingTestModel.objects.get(field1="Apple")
+            self.assertEqual(original, instance)
+
+        self.assertTrue(datastore_get.called)
 
 class ContextCachingTests(TestCase):
     """
@@ -132,12 +327,19 @@ class ContextCachingTests(TestCase):
     """
 
     def test_transactions_get_their_own_context(self):
-        pass
+        with sleuth.watch("djangae.db.backends.appengine.context.ContextStack.push") as context_push:
+            with transaction.atomic():
+                pass
+
+            self.assertTrue(context_push.called)
 
     def test_nested_transaction_doesnt_apply_to_outer_context(self):
         pass
 
     def test_outermost_transaction_applies_all_contexts_on_commit(self):
+        pass
+
+    def test_nested_rollback_doesnt_apply_on_outer_commit(self):
         pass
 
     def test_context_wiped_on_rollback(self):
