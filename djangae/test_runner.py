78e1cfa5f87de2069f95c1f05bcad58ed7eedc47
Luke Benstead <kazade@gmail.com>
2015-03-23 08:52:12 +0000

    Skip tests which don't work with a custom user model

--- djangae/test_runner.py
+++ djangae/test_runner.py
@@ -20,8 +20,16 @@ DJANGO_TESTS_WHICH_REQUIRE_ZERO_PKS = {
     'bulk_create.tests.BulkCreateTests.test_zero_as_autoval'
 }
 
+# These tests only work if you haven't changed AUTH_USER_MODEL
+# This is probably a bug in Django (the tests should use skipIfCustomUser)
+# but I haven't had a chance to see if it's fixed in master (and it's not fixed in
+# 1.7, so this needs to exist either way)
+DJANGO_TESTS_WHICH_REQUIRE_AUTH_USER = {
+    'proxy_models.tests.ProxyModelAdminTests.test_cascade_delete_proxy_model_admin_warning',
+    'proxy_models.tests.ProxyModelAdminTests.test_delete_str_in_model_admin',
+}
 
-DJANGO_TESTS_TO_SKIP = DJANGO_TESTS_WHICH_REQUIRE_ZERO_PKS
+DJANGO_TESTS_TO_SKIP = DJANGO_TESTS_WHICH_REQUIRE_ZERO_PKS.union(DJANGO_TESTS_WHICH_REQUIRE_AUTH_USER)
 
 def init_testbed():
     # We don't initialize the datastore stub here, that needs to be done by Django's create_test_db and destroy_test_db.
