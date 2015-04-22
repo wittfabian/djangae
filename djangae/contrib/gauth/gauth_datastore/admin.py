9082e1a86f5d6291edbfc2b115dc53bdf3d8dee7
Adam Alton <adamalton@gmail.com>
2015-04-22 17:46:08 +0100

    Split admin.py into 2 separate files for gauth_datastore and gauth_sql apps.  #277

--- djangae/contrib/gauth/gauth_datastore/admin.py
+++ djangae/contrib/gauth/gauth_datastore/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
 # DJANGAE
-from djangae.contrib.gauth.models import (
-    GaeUser,
+from djangae.contrib.gauth.gauth_datastore.models import (
     GaeDatastoreUser,
     Group
 )
 
-admin.site.register(GaeUser)
 admin.site.register(GaeDatastoreUser)
 admin.site.register(Group)
