ff834ecbadb743d6d4c9f5817baff64158d4c73c
Luke Benstead <kazade@gmail.com>
2015-05-12 08:47:26 +0100

    Fix some minor logging/setting issues in gauth

--- djangae/contrib/gauth/settings.py
+++ djangae/contrib/gauth/settings.py
@@ -8,4 +8,4 @@ LOGIN_URL = 'djangae_login_redirect'
 
 # This allows you to create User objects for Google-Accounts-based users before they have logged in.
 # When pre-creating a Google user, you must set the `username` to None.  Matching is done by email.
-ALLOW_USER_PRE_CREATION = False
+DJANGAE_ALLOW_USER_PRE_CREATION = False
