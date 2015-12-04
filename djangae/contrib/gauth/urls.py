863c88e6feaa5f64b7b684bde06acbe1b4a37240
Matus Moravcik <matus.moravcik@gmail.com>
2015-12-04 15:35:28 +0000

    Get rid of more deprecated urlpatterns

--- djangae/contrib/gauth/urls.py
+++ djangae/contrib/gauth/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns(
-    'djangae.contrib.gauth.views',
-    url(r'^login_redirect$', 'login_redirect', name='djangae_login_redirect'),
-    url(r'^switch_accounts/$', 'switch_accounts', name='djangae_switch_accounts'),
-)
+from djangae.contrib.gauth import views
+
+
+urlpatterns = [
+    url(r'^login_redirect$', views.login_redirect, name='djangae_login_redirect'),
+    url(r'^switch_accounts/$', views.switch_accounts, name='djangae_switch_accounts'),
+]
--- djangae/urls.py
+++ djangae/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 
 import djangae.contrib.mappers.urls
 from . import views
 
 
-urlpatterns = patterns('djangae.views',
+urlpatterns = [
     url(r'^start$', views.start),
     url(r'^start$', views.stop),
     url(r'^warmup$', views.warmup),
     url(r'^queue/deferred/?$', views.deferred),
     url(r'^internalupload/$', views.internalupload, name='djangae_internal_upload_handler'),
     url(r'^mapreduce/', include(djangae.contrib.mappers.urls))
-)
+]
