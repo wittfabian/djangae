3c764434c21202528ff3dac2d5bb0066bca8bb01
Matus Moravcik <matus.moravcik@gmail.com>
2015-12-04 14:54:49 +0000

    Make urlpatterns 1.10 friendly

--- djangae/urls.py
+++ djangae/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, url, include
+
 import djangae.contrib.mappers.urls
+from . import views
+
 
 urlpatterns = patterns('djangae.views',
-    url(r'^start$', 'start'),
-    url(r'^start$', 'stop'),
-    url(r'^warmup$', 'warmup'),
-    url(r'^queue/deferred/?$', 'deferred'),
-    url(r'^internalupload/$', 'internalupload', name='djangae_internal_upload_handler'),
+    url(r'^start$', views.start),
+    url(r'^start$', views.stop),
+    url(r'^warmup$', views.warmup),
+    url(r'^queue/deferred/?$', views.deferred),
+    url(r'^internalupload/$', views.internalupload, name='djangae_internal_upload_handler'),
     url(r'^mapreduce/', include(djangae.contrib.mappers.urls))
 )
