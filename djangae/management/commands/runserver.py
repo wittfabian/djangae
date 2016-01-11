c4c3ddd3fac9af1d9bac9478b34caf0c4c4e6321
Charlie Morgan <chargrizzled@gmail.com>
2016-01-11 14:07:49 +0000

    More verbose naming

--- djangae/management/commands/runserver.py
+++ djangae/management/commands/runserver.py
@@ -13,10 +13,9 @@ from google.appengine.tools.sdk_update_checker import (
     _VersionList
 )
 
-DJANGAE_IGNORE_REGEXES = []
-if settings.DJANGAE_IGNORE_REGEXES:
-    DJANGAE_IGNORE_REGEXES = [re.compile(regex) for regex in settings.DJANGAE_IGNORE_REGEXES]
-
+DJANGAE_RUNSERVER_IGNORED_FILES_REGEXES = []
+if settings.DJANGAE_RUNSERVER_IGNORED_FILES_REGEXES:
+    DJANGAE_RUNSERVER_IGNORED_FILES_REGEXES = [re.compile(regex) for regex in settings.DJANGAE_RUNSERVER_IGNORED_FILES_REGEXES]
 
 def ignore_file(filename):
     """Report whether a file should not be watched."""
@@ -26,8 +25,9 @@ def ignore_file(filename):
         filename.startswith(watcher_common._IGNORED_PREFIX) or
         any(filename.endswith(suffix) for suffix in watcher_common._IGNORED_FILE_SUFFIXES) or
         watcher_common._IGNORED_REGEX.match(filename) or
-        any(regex.match(filename) for regex in DJANGAE_IGNORE_REGEXES)
+        any(regex.match(filename) for regex in DJANGAE_RUNSERVER_IGNORED_FILES_REGEXES)
     )
+
 
 
 class Command(BaseRunserverCommand):
