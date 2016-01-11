d7104c363ce09a3e7ee68c3213d2f54cae5b218b
Charlie Morgan <chargrizzled@gmail.com>
2016-01-11 13:37:42 +0000

    add skip_files patch

--- djangae/management/commands/runserver.py
+++ djangae/management/commands/runserver.py
@@ -12,6 +12,17 @@ from google.appengine.tools.sdk_update_checker import (
 )
 
 
+def ignore_file(filename):
+    """Report whether a file should not be watched."""
+    from google.appengine.tools.devappserver2 import watcher_common
+    filename = os.path.basename(filename)
+    return(
+        filename.startswith(watcher_common._IGNORED_PREFIX) or
+        any(filename.endswith(suffix) for suffix in watcher_common._IGNORED_FILE_SUFFIXES) or
+        watcher_common._IGNORED_REGEX.match(filename)
+    )
+
+
 class Command(BaseRunserverCommand):
     """
     Overrides the default Django runserver command.
@@ -223,6 +234,15 @@ class Command(BaseRunserverCommand):
                 return sandbox._API_SERVER
 
         from google.appengine.tools.devappserver2 import module
+
+        def fix_watcher_files(regex):
+            from google.appengine.tools.devappserver2 import watcher_common
+            watcher_common._IGNORED_REGEX = regex
+            watcher_common.ignore_file = ignore_file
+
+        regex = sandbox._CONFIG.modules[0].skip_files
+        if regex:
+            fix_watcher_files(regex)
 
         def logging_wrapper(func):
             """
