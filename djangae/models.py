8cffea8e09571f28b8c7787cb29dc0e83a4fcd60
Luke Benstead <kazade@gmail.com>
2016-02-04 11:52:50 +0000

    Make sure the context cache clearing signals are connected

--- djangae/models.py
+++ djangae/models.py
@@ -9,3 +9,11 @@ class CounterShard(models.Model):
 
 # Apply our django patches
 patches.patch()
+
+
+# Make sure we clear the context cache properly
+from djangae.db.backends.appengine.caching import reset_context
+from django.core.signals import request_finished, request_started
+
+request_finished.connect(reset_context, dispatch_uid="request_finished_context_reset")
+request_started.connect(reset_context, dispatch_uid="request_started_context_reset")
