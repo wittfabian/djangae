2239ddc46ea58ae1a13cce72fa84f2e170198aca
Ola Sendecka <olas@potatolondon.com>
2016-03-14 14:02:07 +0000

    More tests of ensure_instances_included
    * test adding many instances at the beginning of the
    queryset.
    * test of deleting many instances

--- djangae/tests/test_utils.py
+++ djangae/tests/test_utils.py
@@ -25,6 +25,7 @@ class EnsureCreatedModel(models.Model):
     def __unicode__(self):
         return u"PK: {}, field1 {}".format(self.pk, self.field1)
 
+
 class EnsureCreatedTests(TestCase):
 
     def test_basic_usage(self):
@@ -78,3 +79,65 @@ class EnsureCreatedTests(TestCase):
 
         new_instance = EnsureCreatedModel.objects.create(pk=8, field1=8)
         self.assertEqual(1, list(ensure_instance_consistent(qs, 8)).count(new_instance))
+
+    def test_add_many_instances(self):
+        for i in xrange(5):
+            EnsureCreatedModel.objects.create(
+                pk=i + 1,
+                field1=i + 5
+            )
+
+        with inconsistent_db():
+            new_instances = []
+            for i in xrange(3):
+                instance = EnsureCreatedModel.objects.create(
+                    pk=i + 7,
+                    field1=i
+                )
+                new_instances.append(instance)
+
+            new_instance_pks = [i.pk for i in new_instances]
+
+            qs = EnsureCreatedModel.objects.all()
+            self.assertEqual(5, len(qs))  # Make sure we don't get the new instance
+            self.assertEqual(8, len(ensure_instances_consistent(qs, new_instance_pks)))
+
+            evaled = [ x for x in ensure_instances_consistent(qs.order_by("field1"), new_instance_pks) ]
+            self.assertEqual(new_instances[0], evaled[0])  # Make sure our instance is correctly positioned
+            self.assertEqual(new_instances[1], evaled[1])  # Make sure our instance is correctly positioned
+            self.assertEqual(new_instances[2], evaled[2])  # Make sure our instance is correctly positioned
+
+        # Now we're in consistent land!
+        self.assertTrue(EnsureCreatedModel.objects.filter(pk=7)[:1])
+        self.assertTrue(EnsureCreatedModel.objects.filter(pk=8)[:1])
+        self.assertEqual(8, len(ensure_instances_consistent(qs, new_instance_pks)))
+
+        # Make sure it's not returned if it was deleted
+        for instance in new_instances:
+            instance.delete()
+        self.assertEqual(5, len(ensure_instances_consistent(qs, new_instance_pks)))
+
+    def test_delete_many_instances(self):
+        for i in xrange(5):
+            EnsureCreatedModel.objects.create(
+                pk=i + 1,
+                field1=i + 5
+            )
+
+        instances_to_delete = []
+        for i in xrange(3):
+            instance = EnsureCreatedModel.objects.create(
+                pk=i + 7,
+                field1=i + 1
+            )
+            instances_to_delete.append(instance)
+
+        instances_to_delete_pks = [i.pk for i in instances_to_delete]
+
+        qs = EnsureCreatedModel.objects.all()
+        self.assertEqual(8, len(ensure_instances_consistent(qs, instances_to_delete_pks)))
+        with inconsistent_db():
+            # Make sure it's not returned if it was deleted
+            for instance in instances_to_delete:
+                instance.delete()
+            self.assertEqual(5, len(ensure_instances_consistent(qs, instances_to_delete_pks)))
