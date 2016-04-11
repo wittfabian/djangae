5506f4eef3ea88700b4353f28acb450433c5d568
Charlie Morgan <chargrizzled@gmail.com>
2016-04-11 09:35:03 -0700

    move qualname to utils

--- djangae/contrib/processing/mapreduce/helpers.py
+++ djangae/contrib/processing/mapreduce/helpers.py
@@ -1,5 +1,4 @@
 import cPickle
-import types
 import pipeline
 from mapreduce import context
 from mapreduce.mapper_pipeline import MapperPipeline
@@ -8,31 +7,7 @@ from mapreduce import pipeline_base
 from django.utils.module_loading import import_string
 from djangae.contrib.processing.mapreduce.input_readers import DjangoInputReader
 
-def qualname(func):
-    if isinstance(func, types.MethodType):
-        return "{cls}.{func}".format(
-            cls=func.im_self.__class__,
-            func=func.im_func.__name__
-        )
-    elif isinstance(func, types.BuiltinMethodType):
-        if not func.__self__:
-            return "{func}".format(
-                func=func.__name__
-            )
-        else:
-            return "{type}.{func}".format(
-                type=func.__self__,
-                func=func.__name__
-            )
-    elif (isinstance(func, types.ObjectType) and hasattr(func, "__call__")) or\
-        isinstance(func, (types.FunctionType, types.BuiltinFunctionType,
-                        types.ClassType, types.UnboundMethodType)):
-        return "{module}.{func}".format(
-            module=func.__module__,
-            func=func.__name__
-        )
-    else:
-        raise ValueError("func must be callable")
+from utils import qualname
 
 
 class DynamicPipeline(pipeline_base.PipelineBase):
