240b89ebc48c6186cf7a595959c03f94ffa039f1
Luke Benstead <kazade@gmail.com>
2016-05-13 13:01:49 +0100

    Fix a docstring

--- djangae/contrib/locking/kinds.py
+++ djangae/contrib/locking/kinds.py
@@ -4,7 +4,7 @@ class LOCK_KINDS(object):
     """ The different kinds of lock which you can use.
         WEAK is not guaranteed to be robust, but can be used for situations where avoiding
         simultaneous code execution is preferable but not critical.
-        STRONG is for where simultaneous code execution is *required*.
+        STRONG is for where preventing simultaneous code execution is *required*.
     """
     WEAK = 'weak'
     STRONG = 'strong'
