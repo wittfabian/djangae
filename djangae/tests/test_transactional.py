8e4a3867a153028b991e3b4abd5eec4e67b98823
Adam Alton <adam@potatolondon.com>
2016-05-26 11:44:13 +0100

    Whitespace tweaks for PEP8 compliance.

--- djangae/tests/test_transactional.py
+++ djangae/tests/test_transactional.py
@@ -113,7 +113,6 @@ class TransactionTests(TestCase):
         with transaction.atomic():
             double_nested_transactional()
 
-
         @db.transactional()
         def something_containing_atomic():
             with transaction.atomic():
@@ -142,7 +141,6 @@ class TransactionTests(TestCase):
 
         self.assertEqual(0, TestUser.objects.count())
 
-
     def test_non_atomic_context_manager(self):
         from .test_connector import TestUser
         existing = TestUser.objects.create(username="existing", field2="exists")
@@ -158,7 +156,7 @@ class TransactionTests(TestCase):
                 self.assertFalse(transaction.in_atomic_block())
 
                 with sleuth.watch("google.appengine.api.datastore.Get") as datastore_get:
-                    TestUser.objects.get(pk=existing.pk) #Should hit the cache, not the datastore
+                    TestUser.objects.get(pk=existing.pk)  # Should hit the cache, not the datastore
 
                 self.assertFalse(datastore_get.called)
 
@@ -175,14 +173,14 @@ class TransactionTests(TestCase):
                         self.assertRaises(TestUser.DoesNotExist, TestUser.objects.get, pk=user2.pk)
 
                         with sleuth.watch("google.appengine.api.datastore.Get") as datastore_get:
-                            TestUser.objects.get(pk=existing.pk) #Should hit the cache, not the datastore
+                            # Should hit the cache, not the Datastore
+                            TestUser.objects.get(pk=existing.pk)
 
                     self.assertFalse(transaction.in_atomic_block())
                     self.assertRaises(TestUser.DoesNotExist, TestUser.objects.get, pk=user2.pk)
 
                 self.assertTrue(TestUser.objects.filter(pk=user2.pk).exists())
                 self.assertTrue(transaction.in_atomic_block())
-
 
     def test_xg_argument(self):
         from .test_connector import TestUser, TestFruit
@@ -215,10 +213,8 @@ class TransactionTests(TestCase):
             TestUser.objects.create(username=_username)
             txn2(_fruit)
 
-
         with self.assertRaises(ValueError):
             txn1("test", "banana")
-
 
     def test_nested_decorator(self):
         # Nested decorator pattern we discovered can cause a connection_stack
