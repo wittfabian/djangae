a75584c359a22204ab5ca442099bb8e561fe409a
David Neale <neale.dj@gmail.com>
2016-12-16 11:19:43 +0000

    Remove unused parameter

--- djangae/contrib/locking/memcache.py
+++ djangae/contrib/locking/memcache.py
@@ -1,15 +1,13 @@
 import time
 from django.core.cache import cache
 
-from .kinds import LOCK_KINDS
-
 
 class MemcacheLock(object):
     def __init__(self, identifier):
         self.identifier = identifier
 
     @classmethod
-    def acquire(cls, identifier, wait=True, steal_after_ms=None, kind=LOCK_KINDS.WEAK):
+    def acquire(cls, identifier, wait=True, steal_after_ms=None):
         # Slightly misusing steal_after_ms here as it's actually used as the expiry time
         # for when the lock is set.
         acquired = cache.add(identifier, 1, steal_after_ms)
