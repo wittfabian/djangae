f61d43a57e49b99cced39515fd0a941f43ef5d8a
Grzes Furga <grzes@potatolondon.com>
2017-01-25 11:11:44 +0100

    Updating gauth docs.

--- djangae/contrib/gauth_datastore/models.py
+++ djangae/contrib/gauth_datastore/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import (
     _user_has_module_perms
 )
 from django.db import models
-from django.db.models import signals
 from django.utils.translation import ugettext_lazy as _
 from django.utils.functional import lazy
 
