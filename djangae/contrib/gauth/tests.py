wittfabian/djangae

commit 78ddecd791e3649372ec6b1dddf4a9a7100689e4

Remove unnecessary `get_session_var` function (and use of).
My guess is that someone used `client._session()` to avoid the behaviour where the session object is recreated each time the attribute is accessed.  But that doesn't actually need avoiding in these cases here.

Then it was changed in 17f8df4fad33b267cc44608e223b64ae30940507 because `client._session` doesn't exist in Django 1.11.

djangae/contrib/gauth/tests.py
@@ -32,10 +32,6 @@ from djangae.contrib.gauth.utils import get_switch_accounts_url
 from djangae.contrib import sleuth
 
 
-def get_session_var(client, key):
-    return client.session[key]
-
-
 class BackendTests(TestCase):
     """ Tests for the AppEngineUserAPIBackend auth backend. """
 
@@ -464,7 +460,7 @@ class SwitchAccountsTests(TestCase):
             # Check that the user is logged in
             expected_user_query = GaeDatastoreUser.objects.filter(username=jekyll.user_id())
             self.assertEqual(len(expected_user_query), 1)
-            self.assertEqual(int(get_session_var(self.client, '_auth_user_id')), expected_user_query[0].pk)
+            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
             # Now call the switch_accounts view, which should give us a redirect to the login page
             response = self.client.get(switch_accounts_url, follow=False)
             self.assertEqual(response.status_code, 302)
@@ -490,7 +486,7 @@ class SwitchAccountsTests(TestCase):
             self.assertEqual(redirect_path, final_destination)
             expected_user_query = GaeDatastoreUser.objects.filter(username=hyde.user_id())
             self.assertEqual(len(expected_user_query), 1)
-            self.assertEqual(int(get_session_var(self.client, '_auth_user_id')), expected_user_query[0].pk)
+            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
 
     @override_settings(DJANGAE_CREATE_UNKNOWN_USER=True)
     def test_switch_to_same_account(self):
@@ -506,7 +502,7 @@ class SwitchAccountsTests(TestCase):
             expected_user_query = GaeDatastoreUser.objects.filter(username=jekyll.user_id())
             self.assertEqual(len(expected_user_query), 1)
 
-            self.assertEqual(int(get_session_var(self.client, '_auth_user_id')), expected_user_query[0].pk)
+            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
             # Call switch_accounts view, which should give a redirect to login url
             response = self.client.get(switch_accounts_url, follow=False)
             self.assertEqual(response.status_code, 302)
@@ -524,7 +520,7 @@ class SwitchAccountsTests(TestCase):
             self.assertEqual(redirect_path, final_destination)
             expected_user_query = GaeDatastoreUser.objects.filter(username=jekyll.user_id())
             self.assertEqual(len(expected_user_query), 1)
-            self.assertEqual(int(get_session_var(self.client, '_auth_user_id')), expected_user_query[0].pk)
+            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
 
 
 class ModelTests(TestCase):

commit 14e83d92da9807a5dc7ca656d2c2d454d8e977b1

Fix silly find-replace-o mistake with `selfclient.session` thing.
I broke this in 78ddecd791e3649372ec6b1dddf4a9a7100689e4.

djangae/contrib/gauth/tests.py
@@ -460,7 +460,7 @@ class SwitchAccountsTests(TestCase):
             # Check that the user is logged in
             expected_user_query = GaeDatastoreUser.objects.filter(username=jekyll.user_id())
             self.assertEqual(len(expected_user_query), 1)
-            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
+            self.assertEqual(int(self.client.session['_auth_user_id']), expected_user_query[0].pk)
             # Now call the switch_accounts view, which should give us a redirect to the login page
             response = self.client.get(switch_accounts_url, follow=False)
             self.assertEqual(response.status_code, 302)
@@ -486,7 +486,7 @@ class SwitchAccountsTests(TestCase):
             self.assertEqual(redirect_path, final_destination)
             expected_user_query = GaeDatastoreUser.objects.filter(username=hyde.user_id())
             self.assertEqual(len(expected_user_query), 1)
-            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
+            self.assertEqual(int(self.client.session['_auth_user_id']), expected_user_query[0].pk)
 
     @override_settings(DJANGAE_CREATE_UNKNOWN_USER=True)
     def test_switch_to_same_account(self):
@@ -502,7 +502,7 @@ class SwitchAccountsTests(TestCase):
             expected_user_query = GaeDatastoreUser.objects.filter(username=jekyll.user_id())
             self.assertEqual(len(expected_user_query), 1)
 
-            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
+            self.assertEqual(int(self.client.session['_auth_user_id']), expected_user_query[0].pk)
             # Call switch_accounts view, which should give a redirect to login url
             response = self.client.get(switch_accounts_url, follow=False)
             self.assertEqual(response.status_code, 302)
@@ -520,7 +520,7 @@ class SwitchAccountsTests(TestCase):
             self.assertEqual(redirect_path, final_destination)
             expected_user_query = GaeDatastoreUser.objects.filter(username=jekyll.user_id())
             self.assertEqual(len(expected_user_query), 1)
-            self.assertEqual(int(self.client['_auth_user_id']), expected_user_query[0].pk)
+            self.assertEqual(int(self.client.session['_auth_user_id']), expected_user_query[0].pk)
 
 
 class ModelTests(TestCase):