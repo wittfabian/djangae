16ca07cb22d2f64476bb8eefb6c1a7c317045e05
jeffrey k eliasen <jeff@jke.net>
2017-06-03 08:23:05 -1000

    class for storing results of tests

--- testprodapp/testapp/models.py
+++ testprodapp/testapp/models.py
@@ -3,6 +3,43 @@ import uuid
 from django.db import models
 
 
+test_result_choices = [(x, x) for x in [
+    'new',
+    'running',
+    'error',
+    'success',
+]]
+
+
+class TestResultManager(models.Manager):
+
+    def get_result(self, name):
+        result, _ = TestResult.objects.get_or_create(name=name)
+        return result
+
+    def set_result(self, name, status, output):
+        result = TestResult.objects.get_result(name=name)
+        result.status = status
+        result.output = output
+        result.save()
+        return result
+
+
+class TestResult(models.Model):
+
+    last_modified = models.DateField(auto_now=True, editable=False)
+    name = models.CharField(max_length=500, editable=False)
+    status = models.CharField(
+        max_length=50,
+        choices=test_result_choices,
+        default=test_result_choices[0][0],
+        editable=False,
+        )
+    output = models.CharField(max_length=500, editable=False)
+
+    objects = TestResultManager()
+
+
 class UuidManager(models.Manager):
 
     def create_entities(self, count=1000):
@@ -14,6 +51,7 @@ class UuidManager(models.Manager):
 
 
 class Uuid(models.Model):
+
     value = models.CharField(max_length=36, default=uuid.uuid4)
 
     objects = UuidManager()
--- testprodapp/testapp/tests.py
+++ testprodapp/testapp/tests.py
@@ -2,7 +2,47 @@ import uuid
 
 from django.test import TestCase
 
+from .models import TestResult
 from .models import Uuid
+
+
+class TestResultTest(TestCase):
+
+    def setUp(self):
+        self.result = TestResult.objects.create(name='test')
+
+    def test_valid_data(self):
+        self.assertIsNotNone(self.result.last_modified)
+        self.assertEqual(self.result.status, 'new')
+        self.assertEqual(self.result.output, '')
+
+
+class TestResultManagerTest(TestCase):
+
+    def setUp(self):
+        self.result_id = TestResult.objects.create(name='test').id
+
+    def test_getter(self):
+        self.assertEqual(TestResult.objects.count(), 1)
+        result = TestResult.objects.get_result('test')
+        self.assertIsNotNone(result)
+        self.assertEqual(result.id, self.result_id)
+
+    def test_getter_new_name(self):
+        TestResult.objects.get_result('test2')
+        self.assertEqual(TestResult.objects.count(), 2)
+
+    def test_setter(self):
+        result = TestResult.objects.set_result('test', 'success', 'abc')
+        self.assertEqual(TestResult.objects.count(), 1)
+        self.assertIsNotNone(result)
+        result = TestResult.objects.get_result('test')
+        self.assertEqual(result.status, 'success')
+        self.assertEqual(result.output, 'abc')
+
+    def test_setter_new_name(self):
+        TestResult.objects.set_result('test2', 'success', 'abc')
+        self.assertEqual(TestResult.objects.count(), 2)
 
 
 class UuidTest(TestCase):
