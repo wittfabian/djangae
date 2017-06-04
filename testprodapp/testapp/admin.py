5a63e55b970888502ebf691624802471c36f8beb
jeffrey k eliasen <jeff@jke.net>
2017-06-03 15:53:23 -1000

    button to bulk-add UUID entities

--- testprodapp/testapp/admin.py
+++ testprodapp/testapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
+from django.conf.urls import patterns
+from django.conf.urls import url
+
+from django.shortcuts import redirect
+
 from .models import TestResult
 from .models import Uuid
 
@@ -23,3 +28,15 @@ class UuidAdmin(admin.ModelAdmin):
         'id',
         'value',
     )
+
+    def get_urls(self):
+        urls = super(UuidAdmin, self).get_urls()
+        my_urls = patterns(
+            '',
+            url(r'^create/$', self.bulk_create_view, name='bulk_create'),
+        )
+        return my_urls + urls
+
+    def bulk_create_view(self, request):
+        Uuid.objects.create_entities()
+        return redirect('admin:testapp_uuid_changelist')
