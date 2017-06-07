25844018013302a4cdb598639d77fc67cc10b2a0
Luke Benstead <kazade@gmail.com>
2017-06-07 13:42:32 +0100

    Fix SDK path issues

--- testprodapp/manage.py
+++ testprodapp/manage.py
@@ -2,10 +2,11 @@
 import os
 import sys
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp.settings")
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp.settings')
-    sys.path.insert(
-        1, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libs'))
+    from fix_path import fix_path
+    fix_path()
+
     from djangae.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
