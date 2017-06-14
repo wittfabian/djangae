5490bcb616fdcda873d91721037a23c18f540e82
Luke Benstead <kazade@gmail.com>
2017-06-14 16:26:54 +0100

    Don't append PRIMARY KEY to columns, the syntax of Spanner puts it at the end

--- djangae/db/backends/spanner/schema.py
+++ djangae/db/backends/spanner/schema.py
@@ -4,4 +4,11 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 
 class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
-    pass
+    def column_sql(self, model, field, include_default=False):
+        sql, params = super(DatabaseSchemaEditor, self).column_sql(model, field, include_default)
+
+        # Cloud Spanner doesn't allow specifying a primary key alongside the column
+        # it must instead by specified alongside indexes at the end of the create table statement
+        sql = sql.replace(" PRIMARY KEY", "")
+
+        return sql, params
