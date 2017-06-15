f1591735f218c769b0d00ffc0e17a21bf02bd1b8
Luke Benstead <kazade@gmail.com>
2017-06-15 12:40:44 +0100

    Ensure that string/bytes types always have the correct type on insert

--- djangae/db/backends/spanner/compiler.py
+++ djangae/db/backends/spanner/compiler.py
@@ -1,5 +1,6 @@
 
 from django.db.models.sql import compiler
+from django.utils.encoding import force_text, force_bytes
 
 
 class SQLCompiler(compiler.SQLCompiler):
@@ -7,7 +8,29 @@ class SQLCompiler(compiler.SQLCompiler):
 
 
 class SQLInsertCompiler(compiler.SQLInsertCompiler, SQLCompiler):
-    pass
+    def as_sql(self):
+        queries = super(SQLInsertCompiler, self).as_sql()
+
+        has_fields = bool(self.query.fields)
+        if not has_fields:
+            return queries
+
+        # For string/bytes types we MUST ensure that the values are the correct type
+        # (e.g. unicode for string, str for bytes, or str/bytes on Py3). This is because
+        # Spanner doesn't implicitly cast from one to the other and the connector
+        # has to indicate which type each field is
+
+        field_types = [x.db_type(self.connection) for x in self.query.fields]
+
+        # Go throug the queries, enforce the correct types
+        for sql, params in queries:
+            for i, field_type in enumerate(field_types):
+                if field_type.startswith("STRING("):
+                    params[i] = force_text(params[i])
+                elif field_type.startswith("BYTES("):
+                    params[i] = force_bytes(params[i])
+
+        return queries
 
 
 class SQLDeleteCompiler(compiler.SQLDeleteCompiler, SQLCompiler):
