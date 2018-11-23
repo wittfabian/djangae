12a754e23175820424bf9f3f0e2c1d5390bab51c
Danny Milsom <danny@potatolondon.com>
2018-11-23 15:09:17 +0000

    Update tests to reflect API client usage

--- djangae/contrib/backup/tests/test_tasks.py
+++ djangae/contrib/backup/tests/test_tasks.py
@@ -11,11 +11,12 @@ from djangae.test import TestCase
 from djangae.contrib.backup.tasks import (
     _get_valid_export_models,
     backup_datastore,
-    SERVICE_URL
+    SERVICE_URL,
+    AUTH_SCOPES,
 )
 
 from google.appengine.api import app_identity
-
+from google.auth import app_engine
 
 def mock_get_app_models(**kwargs):
     return [
@@ -46,31 +47,18 @@ class GetValidExportModelsTestCase(TestCase):
         self.assertNotIn('django_admin_log', valid_models)
 
 
-class BackupDatastoreTestCase(TestCase):
-    """Tests for djangae.contrib.backup.tasks.backup_datastore"""
+class BackupTestCase(TestCase):
 
     @override_settings(DJANGAE_BACKUP_ENABLED=True)
-    @sleuth.switch('django.apps.apps.get_models', mock_get_app_models)
     def test_ok(self):
-        kinds = ['admin_logentry', 'gauth_datastore_gaedatastoreuser']
-        with sleuth.watch('djangae.contrib.backup.tasks.urlfetch.fetch') as mock_fn:
-            backup_datastore(kinds)
-
-        self.assertTrue(mock_fn.called)
-        call_args = mock_fn.calls[0][1]
-        self.assertEqual(
-            call_args['url'],
-            SERVICE_URL.format(app_id=application_id())
-        )
-        self.assertItemsEqual(json.loads(call_args['payload'])['entity_filter']['kinds'], kinds)
-        self.assertEqual(json.loads(call_args['payload'])['project_id'], application_id())
-
-    @override_settings(DJANGAE_BACKUP_ENABLED=True)
-    @sleuth.switch('django.apps.apps.get_models', mock_get_app_models)
-    def test_invalid_kinds(self):
-        kinds = ['not_real_model']
-        with sleuth.watch('djangae.contrib.backup.tasks.urlfetch.fetch') as mock_fn:
-            backup_datastore(kinds=kinds)
-
-        self.assertFalse(mock_fn.called)
-
+        """Lightweight end-to-end flow test of backup_datastore."""
+        with sleuth.switch(
+            'djangae.contrib.backup.tasks._get_authentication_credentials',
+            lambda: app_engine.Credentials(scopes=AUTH_SCOPES)
+        ):
+            with sleuth.switch(
+                'googleapiclient.http.HttpRequest.execute', lambda x: True
+            ) as mock_fn:
+                kinds = ['gauth_datastore_gaedatastoreuser']
+                backup_datastore(kinds=kinds)
+                self.assertTrue(mock_fn.called)
