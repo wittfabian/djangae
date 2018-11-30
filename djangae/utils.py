511c3e38873fa2553f1198fb342bf3e9e3af66e9
Luke Benstead <kazade@gmail.com>
2018-11-30 19:36:39 +0000

    Always log the tracebacks if we're retrying

--- djangae/utils.py
+++ djangae/utils.py
@@ -125,6 +125,10 @@ def retry(func, *args, **kwargs):
                     logger.error("Ran out of attempts while retrying function")
                     raise  # Re-raise original exception
 
+                # The location of the errors on each attempt may change, so we log
+                # each one
+                logger.exception("Exception during retry attempt. Will retry.")
+
                 logger.info("Retrying function: %s(%s, %s) - %s", func, args, kwargs, exc)
                 time.sleep(timeout_ms * 0.001)
                 timeout_ms *= 2
