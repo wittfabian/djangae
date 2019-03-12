b93d8599c53edf42045b9c7f4a471282f5398689
Luke Benstead <kazade@gmail.com>
2019-03-12 12:05:15 +0000

    Remove a lot of stuff that isn't needed on the Python 3 runtime

--- djangae/apps.py
+++ djangae/apps.py
@@ -11,13 +11,9 @@ class DjangaeConfig(AppConfig):
         from .patches import json
         json.patch()
 
-        from djangae.db.backends.appengine.caching import reset_context
         from djangae.db.migrations.signals import check_migrations
-        from django.core.signals import request_finished, request_started
         from django.db.models.signals import pre_migrate
 
-        request_finished.connect(reset_context, dispatch_uid="request_finished_context_reset")
-        request_started.connect(reset_context, dispatch_uid="request_started_context_reset")
         pre_migrate.connect(check_migrations, dispatch_uid="pre_migrate_check_connections")
 
         self._check_content_types()
--- djangae/contrib/gauth/common/models.py
+++
@@ -1,8 +0,0 @@
-import warnings
-
-warnings.warn(
-    "djangae.contrib.gauth.common is deprecated, please import from djangae.contrib.gauth "
-    "directly instead."
-)
-
-from djangae.contrib.gauth import models, backends, validators
