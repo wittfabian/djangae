590434b6c82100235c48906bdda0149840c44b60
Luke Benstead <kazade@gmail.com>
2020-01-20 14:35:07 +0000

    Remove contrib.gauth and contrib.contenttypes
    gauth will return at some point backed by oauth, but without the users
    API there is not much salvageable.
    
    contenttypes existed for eventual consistency reasons which don't
    affect Firestore in Datastore mode.

--- djangae/contrib/backup/tests/test_tasks.py
+++ djangae/contrib/backup/tests/test_tasks.py
@@ -1,9 +1,8 @@
 import json
-
+from django.db import models
 from django.test import override_settings
 from django.contrib.admin.models import LogEntry
 
-from djangae.contrib.gauth_datastore.models import GaeDatastoreUser
 from djangae.contrib import sleuth
 from djangae.environment import application_id
 from djangae.test import TestCase
@@ -15,13 +14,18 @@ from djangae.contrib.backup.tasks import (
     AUTH_SCOPES,
 )
 
-from google.appengine.api import app_identity
 from google.auth import app_engine
+
+
+class MockUser(models.Model):
+    class Meta:
+        app_label = "backup"
+
 
 def mock_get_app_models(**kwargs):
     return [
         LogEntry,
-        GaeDatastoreUser,
+        MockUser,
     ]
 
 
@@ -32,18 +36,18 @@ class GetValidExportModelsTestCase(TestCase):
     @sleuth.switch('django.apps.apps.get_models', mock_get_app_models)
     def test_models_filtered(self):
         valid_models = _get_valid_export_models(
-            ['django_admin_log', 'gauth_datastore_gaedatastoreuser']
+            ['django_admin_log', 'backup_mockuser']
         )
         self.assertNotIn('django_admin_log', valid_models)
-        self.assertIn('gauth_datastore_gaedatastoreuser', valid_models)
+        self.assertIn('backup_mockuser', valid_models)
 
     @override_settings(DJANGAE_BACKUP_EXCLUDE_APPS=['django'])
     @sleuth.switch('django.apps.apps.get_models', mock_get_app_models)
     def test_apps_filtered(self):
         valid_models = _get_valid_export_models(
-            ['django_admin_log', 'gauth_datastore_gaedatastoreuser']
+            ['django_admin_log', 'backup_mockuser']
         )
-        self.assertIn('gauth_datastore_gaedatastoreuser', valid_models)
+        self.assertIn('backup_mockuser', valid_models)
         self.assertNotIn('django_admin_log', valid_models)
 
 
@@ -59,6 +63,6 @@ class BackupTestCase(TestCase):
             with sleuth.switch(
                 'googleapiclient.http.HttpRequest.execute', lambda x: True
             ) as mock_fn:
-                kinds = ['gauth_datastore_gaedatastoreuser']
+                kinds = ['backup_mockuser']
                 backup_datastore(kinds=kinds)
                 self.assertTrue(mock_fn.called)
