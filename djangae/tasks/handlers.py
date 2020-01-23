ce12ad1a373f924855b0db7425f68f63c83c30ab
Luke Benstead <kazade@gmail.com>
2020-01-23 16:30:45 +0000

    Add a middleware for setting task environment vars
    These used to exist on the GAE Python 2 runtime and it
    makes sense for them to be available anyway. A middleware
    seems a sensible place for them.

--- djangae/tasks/handlers.py
+++ djangae/tasks/handlers.py
@@ -1,18 +1,14 @@
-import os
 import pickle
+
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from djangae.environment import task_only
 
 
+@csrf_exempt
+@task_only
 def deferred_handler(request):
-    # Make sure we set the appengine headers in the environment from the
-    # request. This may happen on live by default (not sure, docs are shaky)
-    # but we set them if they're not anyway for local dev
-    os.environ.setdefault("HTTP_X_APPENGINE_TASKNAME", request.META.get("HTTP_X_APPENGINE_TASKNAME", ""))
-    os.environ.setdefault("HTTP_X_APPENGINE_QUEUENAME", request.META.get("HTTP_X_APPENGINE_QUEUENAME", ""))
-    os.environ.setdefault("HTTP_X_APPENGINE_TASKEXECUTIONCOUNT", request.META.get(
-        "HTTP_X_APPENGINE_TASKEXECUTIONCOUNT", "")
-    )
-
     callback, args, kwargs = pickle.loads(request.body)
     callback(*args, **kwargs)
 
