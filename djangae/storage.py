89d94a0343df6fe9a649d61b6d880e24b314100e
Alessandro Artoni <artoale@potatolondon.com>
2020-02-07 15:30:39 +0000

    Initial storage work

---
+++ djangae/storage.py
@@ -0,0 +1,117 @@
+from enum import Enum
+import os
+from io import BytesIO, UnsupportedOperation
+
+import requests
+from django.core.files.storage import File, Storage
+from google.api_core.exceptions import Conflict
+from google.cloud.exceptions import NotFound
+
+
+# TODO: make this configurable
+def _get_storage_client(http=None):
+    """Gets a python storage client"""
+    os.environ["STORAGE_EMULATOR_HOST"] = "http://localhost:9023"
+
+    if not http:
+        http = requests.Session()
+    from google.cloud import storage
+    return storage.Client(
+        project="[PROJECT]",
+        _http=http,
+        client_options={"api_endpoint": "http://localhost:9023"},
+    )
+
+
+class CloudStorageFile(File):
+    def __init__(self, bucket, name=None, mode='rb'):
+        self._name = name
+        self._mode = mode
+        self._blob = bucket.blob(name)
+
+    def read(self, num_bytes=None):
+        if 'r' not in self._mode:
+            raise UnsupportedOperation("File open in '{}' is not readable".format(self._mode))
+        if num_bytes:
+            raise NotImplementedError("Specified argument 'num_bytes: {}' not supported".format(num_bytes))
+
+        f = BytesIO()
+        self._blob.download_to_file(f)
+        return f.getvalue()
+
+    def write(self, content):
+        raise NotImplementedError('Write of CloudStorageFile object not currently supported.')
+
+
+class CloudStorage(Storage):
+    """
+        Google Cloud Storage backend, set this as your default backend
+        for ease of use, you can specify and non-default bucket in the
+        constructor.
+
+        You can modify objects access control by changing google_acl
+        attribute to one of mentioned by docs (XML column):
+        https://cloud.google.com/storage/docs/access-control/lists?hl=en#predefined-acl
+    """
+    def __init__(self, bucket_name="test-bucket", google_acl=None):
+        self._bucket_name = bucket_name
+        self._client = None
+        self._bucket = None
+        self._google_acl = google_acl
+
+    @property
+    def client(self):
+        if self._client is None:
+            self._client = _get_storage_client()
+        return self._client
+
+    @property
+    def bucket(self):
+        if not self._bucket:
+            try:
+                self._bucket = self.client.create_bucket(self._bucket_name)
+            except Conflict:
+                self._bucket = self.client.get_bucket(self._bucket_name)
+        return self._bucket
+
+    def get_valid_name(self, name):
+        # App Engine doesn't properly deal with "./" and a blank upload_to argument
+        # on a filefield results in ./filename so we must remove it if it's there.
+        if name.startswith("./"):
+            name = name.replace("./", "", 1)
+        return name
+
+    def exists(self, name):
+        return bool(self.bucket.get_blob(name))
+
+    def _save(self, name, content):
+        # Not sure why, but it looks like django is not actually calling this
+        name = self.get_valid_name(name)
+        blob = self.bucket.blob(name)
+        blob.upload_from_file(
+            content.file, size=content.size, predefined_acl=self._google_acl
+        )
+        return name
+
+    def _open(self, name, mode="r"):
+        return CloudStorageFile(self._bucket, name=name, mode=mode)
+
+    def size(self, name):
+        blob = self.bucket.get_blob(name)
+        if blob is None:
+            raise NotFound("File {} does not exists".format(name))
+        return blob.size
+
+    def delete(self, name):
+        return self._bucket.delete_blob(name)
+
+    def url(self, name):
+        return get_public_url(name)
+
+
+def get_public_url(name):
+    return "http://localhost:9023/test-bucket/{}".format(name)
+
+
+def has_cloudstorage():
+    return True
---
+++ djangae/tests/test_storage.py
@@ -0,0 +1,146 @@
+# coding: utf-8
+# STANDARD LIB
+import os
+from unittest import skipIf
+
+# THIRD PARTY
+import requests
+from django.core.files.base import (
+    ContentFile,
+    File,
+)
+from django.db import models
+from django.test.utils import override_settings
+from django.utils import six
+
+# DJANGAE
+from djangae.contrib import sleuth
+from djangae.storage import (
+    CloudStorage,
+    has_cloudstorage,
+)
+from djangae.test import TestCase
+
+
+class ModelWithTextFile(models.Model):
+    class Meta:
+        app_label = "djangae"
+
+    text_file = models.FileField()
+
+
+class ModelWithUploadTo(models.Model):
+    class Meta:
+        app_label = "djangae"
+
+    text_file = models.FileField(upload_to="documents/")
+
+
+@skipIf(not has_cloudstorage, "Cloud Storage not available")
+class CloudStorageTests(TestCase):
+    def setUp(self):
+        # TODO: this should be more generic
+        requests.get('http://localhost:9023/wipe')
+        return super().setUp()
+
+    @override_settings(CLOUD_STORAGE_BUCKET='test_bucket')
+    def test_basic_actions(self):
+        content = b'content'
+        storage = CloudStorage()
+        name = u'tmp.ąćęłńóśźż.马铃薯.zip'
+
+        f = ContentFile(content, name='my_file')
+        filename = storage.save(name, f)
+        self.assertIsInstance(filename, six.string_types)
+        self.assertTrue(filename.endswith(name))
+
+        self.assertTrue(storage.exists(filename))
+        self.assertEqual(storage.size(filename), len(content))
+        url = storage.url(filename)
+        self.assertIsInstance(url, six.string_types)
+        self.assertNotEqual(url, '')
+
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, content)
+
+        f = storage.open(filename)
+        self.assertIsInstance(f, File)
+        self.assertEqual(f.read(), content)
+
+        # Delete it
+        storage.delete(filename)
+        self.assertFalse(storage.exists(filename))
+
+    @override_settings(CLOUD_STORAGE_BUCKET='test_bucket')
+    def test_dotslash_prefix(self):
+        storage = CloudStorage()
+        name = './my_file'
+        f = ContentFile(b'content')
+        filename = storage.save(name, f)
+        self.assertEqual(filename, name.lstrip("./"))
+
+    @override_settings(CLOUD_STORAGE_BUCKET='test_bucket')
+    def test_supports_nameless_files(self):
+        storage = CloudStorage()
+        f2 = ContentFile(b'nameless-content')
+        storage.save('tmp2', f2)
+
+    @override_settings(CLOUD_STORAGE_BUCKET='test_bucket')
+    def test_new_objects_get_the_default_acl(self):
+        storage = CloudStorage()
+        filename = 'example.txt'
+        fileobj = ContentFile(b'content')
+
+        # TODO: clean this up somehow?
+        os.environ["STORAGE_EMULATOR_HOST"] = "http://localhost:9023"
+        with sleuth.watch('google.cloud.storage.blob.Blob.upload_from_file') as upload_func:
+            storage.save(filename, fileobj)
+
+        self.assertTrue(storage.exists(filename))
+        self.assertIsNone(upload_func.calls[0].kwargs['predefined_acl'])
+
+    @override_settings(CLOUD_STORAGE_BUCKET='test_bucket')
+    def test_new_objects_with_an_explicit_acl(self):
+        storage = CloudStorage(google_acl='publicRead')
+        filename = 'example.txt'
+        fileobj = ContentFile(b'content', name=filename)
+
+        # TODO: clean this up somehow?
+        os.environ["STORAGE_EMULATOR_HOST"] = "http://localhost:9023"
+        with sleuth.watch('google.cloud.storage.blob.Blob.upload_from_file') as upload_func:
+            storage.save(filename, fileobj)
+
+        self.assertTrue(storage.exists(filename))
+        self.assertEqual(
+            upload_func.calls[0].kwargs['predefined_acl'],
+            'publicRead',
+        )
+
+    @override_settings(
+        CLOUD_STORAGE_BUCKET='test_bucket',
+        DEFAULT_FILE_STORAGE='djangae.storage.CloudStorage',
+    )
+    def test_works_with_text_file_fields(self):
+        content = b"content"
+        instance = ModelWithTextFile(
+            text_file=ContentFile(content, name="my_file")
+        )
+
+        instance.save()
+        fetched = ModelWithTextFile.objects.get()
+        self.assertEqual(fetched.text_file.read(), content)
+
+    @override_settings(
+        CLOUD_STORAGE_BUCKET='test_bucket',
+        DEFAULT_FILE_STORAGE='djangae.storage.CloudStorage',
+    )
+    def test_works_with_upload_to(self):
+        content = b"content"
+        instance = ModelWithUploadTo(
+            text_file=ContentFile(content, name="my_file")
+        )
+
+        instance.save()
+        fetched = ModelWithTextFile.objects.get()
+        self.assertEqual(fetched.text_file.read(), content)
