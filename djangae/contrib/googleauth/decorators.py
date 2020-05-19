07eb87a29dd9d9a58c220dc40c83fc356c31b650
Luke Benstead <kazade@gmail.com>
2020-05-19 13:15:04 +0100

    Allow passing scope to login as a GET param, but check against a whitelist

--- djangae/contrib/googleauth/decorators.py
+++ djangae/contrib/googleauth/decorators.py
@@ -2,51 +2,23 @@ import functools
 
 from django.conf import settings
 from django.http import HttpResponseRedirect
-from django.urls import reverse
-
-from .credentials import create_oauth2_flow
-from .state import generate_state
-
-
-_DEFAULT_OAUTH_SCOPES = [
-    "openid",
-    "profile",
-    "email"
-]
+from django.urls import (
+    redirect,
+    reverse,
+)
 
 
-def oauth_login_required(function, scopes=None):
-    scopes = scopes or getattr(settings, "GOOGLEAUTH_OAUTH_SCOPES", _DEFAULT_OAUTH_SCOPES)
-
+def oauth_scopes_required(function, scopes):
     @functools.wraps(function)
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated():
             return function(request, *args, **kwargs)
         else:
-            flow = create_oauth2_flow(scopes)
-
-            # Redirect the user to the oauth login
-            original_url = "%s://%s%s" % (
-                request.scheme(),
-                request.META['HTTP_HOST'],
-                request.get_full_path()
-            )
-
-            flow.redirect_uri = "%s://%s%s" % (
-                request.scheme(),
-                request.META['HTTP_HOST'],
-                reverse("googleauth_oauth2callback")
-            )
-
-            authorization_url, state = flow.authorization_url(
-                access_type='offline',
-                include_granted_scopes='true',
-                state=generate_state(
-                    request,
-                    redirect_to=original_url
+            # Redirect to the oauth login view (which will then redirect to the oauth flow)
+            return HttpResponseRedirect(
+                reverse("oauth_login_trigger") + "?next=%s" % (
+                    request.get_full_path()
                 )
             )
-
-            return HttpResponseRedirect(authorization_url)
 
     return wrapper
--- djangae/contrib/googleauth/views.py
+++ djangae/contrib/googleauth/views.py
@@ -1,9 +1,77 @@
-from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.http import (
+    Http404,
+    HttpResponseRedirect,
+)
+from django.urls import reverse
 
 from .credentials import create_oauth2_flow
-from .models import OAuthUserSession, _OAUTH_USER_SESSION_SESSION_KEY
-from .state import check_state
-from django.http import Http404
+from .models import (
+    _OAUTH_USER_SESSION_SESSION_KEY,
+    OAuthUserSession,
+)
+from .state import (
+    check_state,
+    generate_state,
+)
+
+_DEFAULT_OAUTH_SCOPES = [
+    "openid",
+    "profile",
+    "email"
+]
+
+_DEFAULT_WHITELISTED_SCOPES = _DEFAULT_OAUTH_SCOPES[:]
+
+
+def login(request):
+    """
+        This view should be set as your login_url for using OAuth
+        authentication. It will trigger the main oauth flow.
+    """
+    WHITELISTED_SCOPES = getattr(settings, "GOOGLE_OAUTH_SCOPE_WHITELIST", _DEFAULT_WHITELISTED_SCOPES)
+    DEFAULT_SCOPES = getattr(settings, "GOOGLEAUTH_OAUTH_SCOPES", _DEFAULT_OAUTH_SCOPES)
+
+    destination = request.GET.get("next", "/")
+    scopes = request.GET.get("scopes").split("%20")
+
+    # This is a security check to make sure we only ask for access
+    # to scopes that we've whitelisted. Just in-case the querystring
+    # parameter has been manipulated maliciously in some way
+    if not all(x in WHITELISTED_SCOPES for x in scopes):
+        raise Http404(
+            "Not all scopes were whitelisted for the application."
+        )
+
+    # If no scopes were provided, then we revert to the default
+    # scopes
+    scopes = scopes or DEFAULT_SCOPES
+
+    flow = create_oauth2_flow(scopes)
+
+    # Redirect the user to the oauth login
+    original_url = "%s://%s%s" % (
+        request.scheme(),
+        request.META['HTTP_HOST'],
+        destination
+    )
+
+    flow.redirect_uri = "%s://%s%s" % (
+        request.scheme(),
+        request.META['HTTP_HOST'],
+        reverse("googleauth_oauth2callback")
+    )
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true',
+        state=generate_state(
+            request,
+            redirect_to=original_url
+        )
+    )
+
+    return HttpResponseRedirect(authorization_url)
 
 
 def oauth2callback(request):
