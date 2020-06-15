01f69c85d93ad5a70f2d212ff4e38485624e327f
Luke Benstead <kazade@gmail.com>
2020-06-15 14:32:07 +0100

    Update __init__.py

--- djangae/__init__.py
+++ djangae/__init__.py
@@ -8,6 +8,6 @@ if extra_library_path not in sys.path:
 default_app_config = 'djangae.apps.DjangaeConfig'
 
 __title__ = 'Djangae'
-__version__ = '2.0.0'
+__version__ = '1.0.1'
 
 VERSION = __version__
